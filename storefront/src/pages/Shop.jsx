729a187121cbcdd70abb914ea0fb8479bed9c279
Edmond Balika <balikaem@gmail.com>
2026-07-29 01:12:25 +0000

    fixed filter panel and transition improvements

--- storefront/src/pages/Shop.jsx
+++ storefront/src/pages/Shop.jsx
@@ -183,7 +183,7 @@ export default function Shop({ products, onProductClick, searchQuery, loading })
       gap: '24px'
     }}>
       {/* Desktop Filter Sidebar */}
-      <aside className="desktop-filters card glass" style={{ height: 'fit-content', position: 'sticky', top: '24px' }}>
+      <aside className="desktop-filters card glass" style={{ height: 'fit-content', position: 'sticky', top: '24px', maxHeight: 'calc(100vh - 100px)', overflowY: 'auto' }}>
         <FilterPanel
           filters={filters}
           setFilters={setFilters}
